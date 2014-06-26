046735f70b47a14198d1019dae1d24281d7691f4
Rudolf Hoefler <rudolf.hoefler@gmail.com>
2014-06-26 09:40:51 +0200

    data dir in hdf added

--- af/hdfio/hdfwriter.py
+++ af/hdfio/hdfwriter.py
@@ -22,12 +22,12 @@ class HdfCache(object):
     def __init__(self, feature_names, gallery_dtype):
 
         self.gallery = None
-        self.data = None
+        self.bbox = None
         self.features = None
         self.contours = defaultdict(list)
 
         self._dt_gallery = gallery_dtype
-        self._dt_data = [('label', np.uint32), ('x', np.uint16),
+        self._dt_bbox = [('label', np.uint32), ('x', np.uint16),
                          ('y', np.uint16), ('top', np.uint16),
                          ('bottom', np.uint16), ('left', np.uint16),
                          ('right', np.uint16)]
@@ -39,14 +39,14 @@ class HdfCache(object):
         nobj = len(objectsdict)
         nftrs = len(objectsdict.feature_names)
 
-        data = np.empty((nobj, ), dtype=self._dt_data)
+        bbox = np.empty((nobj, ), dtype=self._dt_bbox)
         features = np.empty((nobj, ), dtype=self._dt_features)
         gallery = np.empty(objectsdict.gallery_shape, dtype=self._dt_gallery)
 
         # need to cast to the correct type
         for i, (label, obj) in enumerate(objectsdict.iteritems()):
-            data[i] = np.array((label, ) + obj.center + obj.bbox,
-                               dtype=self._dt_data)
+            bbox[i] = np.array((label, ) + obj.center + obj.bbox,
+                               dtype=self._dt_bbox)
             features[i] = obj.features.astype(np.float32)
             gallery[:, :, :, i] = obj.gallery_image.astype(np.uint16)
 
@@ -54,23 +54,23 @@ class HdfCache(object):
                 self.contours[cname].append(
                     np.array(contour, dtype=np.uint16).T)
 
-        if self.data is self.features is self.gallery:
-            self.data = data
+        if self.bbox is self.features is self.gallery:
+            self.bbox = bbox
             self.features = features
             self.gallery = gallery
         else:
-            self.data = np.append(self.data, data)
+            self.bbox = np.append(self.bbox, bbox)
             self.features = np.append(self.features, features)
             self.gallery = np.concatenate((self.gallery, gallery), axis=3)
 
 
 class HdfWriter(object):
 
-    IMAGES = "/images"
-    CONTOURS = "/contours"
-    GALLERY = "/gallery"
-    DATA = "/data"
-    FEATURES = "/features"
+    IMAGES = "/data/images"
+    CONTOURS = "/data/contours"
+    GALLERY = "/data/gallery"
+    BBOX = "/data/bbox"
+    FEATURES = "/data/features"
 
     def __init__(self, filename):
         self._file = h5py.File(filename, "w")
@@ -106,7 +106,7 @@ class HdfWriter(object):
         # hdf5 allows only 64kb of header metadata
         try:
             dset = self._file.create_dataset(
-                self.DATA, data=self._cache.data)
+                self.BBOX, data=self._cache.bbox)
             dset = self._file.create_dataset(
                 self.FEATURES, data=self._cache.features)
             dset = self._file.create_dataset(
