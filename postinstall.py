43942c175d01709313c27bd04e08e9d8a7579465
Rudolf Hoefler <rudolf.hoefler@gmail.com>
2014-10-02 13:30:46 +0200

    setupscript for adpopted for bdist_wininst command

---
+++ postinstall.py
@@ -0,0 +1,45 @@
+"""
+postinstall.py
+
+Create a shortcut in the start menu after installation.
+"""
+
+__author__ = 'rudolf.hoefler@gmail.com'
+__licence__ = 'LGPL'
+
+import os
+import sys
+
+def mkshortcut(target, description, link_file, *args,**kw):
+    """make a shortcut if it doesn't exist, and register its creation"""
+    create_shortcut(target, description, link_file,*args,**kw)
+    file_created(link_file)
+
+def install():
+    pythonw = os.path.join(sys.prefix, 'pythonw.exe')
+    start_menu = get_special_folder_path('CSIDL_COMMON_PROGRAMS')
+
+   # create directory in the start menu
+    if not os.path.isdir(start_menu):
+        os.mkdir(start_menu)
+        directory_created(start_menu)
+
+    link = os.path.join(start_menu, 'AnnotationTool.lnk')
+    command = os.path.join(sys.prefix, 'Scripts', 'AnnotationTool.py')
+    icon = os.path.join(sys.prefix, 'share', 'AnnotationTool', 'annotationtool.ico')
+    workdir = "%HOMEDRIVE%%HOMEPATH%"
+    mkshortcut(pythonw, 'AnnotationTool', link, command, workdir, icon)
+
+def remove():
+    pass
+
+if sys.argv[1] == '-install':
+    try:
+        install()
+    except Exception as e:
+        fp = open(os.path.join(os.path.expanduser("~"),
+                               "AnnotationToolInstallError.txt"), 'a')
+        fp.write(e.message)
+        raise
+else:
+    remove()
--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ __author__ = 'rudolf.hoefler@gmail.com'
 __licence__ = 'GPL'
 
 
+import glob
 from os.path import join
 from distutils.core import setup
 
@@ -25,8 +26,11 @@ setup(name='AnnotationTool',
       author='Rudolf Hoefler',
       author_email='rudolf.hoefler@gmail.com',
       packages = build_helpers.find_submodules("./af", "af"),
+      data_files=[(join('share','AnnotationTool'), glob.glob('./qrc/*.ico'))],
       package_data = {'af': ['gui/*.ui', 'gui/sidebar/*.ui']},
-      scripts = ['AnnotationTool.py'],
+      scripts = ['AnnotationTool.py', 'postinstall.py'],
       cmdclass = {'pyrcc': build_helpers.PyRcc,
                   'build': build_helpers.Build},
-      options = {'pyrcc': pyrcc_opts, })
+      options = {'pyrcc': pyrcc_opts,
+                 'bdist_wininst':
+                     {'install_script': 'postinstall.py'}})
