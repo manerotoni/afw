61429d6e32f992b4cfa4082cecb149f6d4435388
Rudolf Hoefler <rudolf.hoefler@gmail.com>
2014-12-15 18:14:51 +0100

    VC

--- qhelp/demo.py
+++ qhelp/demo.py
@@ -1,9 +1,10 @@
 
 from PyQt4 import QtGui
 from PyQt4 import QtHelp
+from PyQt4.QtCore import Qt
 
 
-class HelpBrowser(QtGui.TextBrowser):
+class HelpBrowser(QtGui.QTextBrowser):
 
     SCHEME = 'qthelp'
 
@@ -11,10 +12,10 @@ class HelpBrowser(QtGui.TextBrowser):
         super(HelpBrowser, self).__init__(*args, **kw)
         self.helpengine = helpengine
 
-    def loadResource(type_, url):
+    def loadResource(self, type_, url):
         if url.scheme() == self.SCHEME:
-            return return self.helpEngine.fileData(url));
-        else
+            return self.helpengine.fileData(url);
+        else:
             return super(HelpBrowser, self).loadResource(type_, url);
 
 
@@ -23,36 +24,18 @@ class HelpViewer(QtGui.QWidget):
     def __init__(self):
         super(HelpViewer, self).__init__()
 
-        splitter = QtGui.QSplitter(Qt.Horizontal)
-        hengine = QtHelp.QHelpEngine("./annotationtool.qch")
-        hbrowser = HelpBrowser(engine)
+        splitter = QtGui.QSplitter(Qt.Horizontal, self)
+        splitter.setStretchFactor(1, 1)
+        hengine = QtHelp.QHelpEngine("./annotationtool.qhc")
+        hengine.setupData()
 
-        # cw = engine.contentWidget()
-        # iw = engine.indexWidget()
+        hbrowser = HelpBrowser(hengine)
 
-        # vbox.addWidget(cw)
-        # vbox.addWidget(iw)
+        hengine.contentWidget().linkActivated.connect(
+        hbrowser.setSource)
 
-
-        engine.setupFinished.connect(cw.show)
-        engine.setupFinished.connect(iw.show)
-        engine.setupData()
-
-        search = engine.searchEngine()
-
-        import pdb; pdb.set_trace()
-
-        # get all file references for the identifier
-        links = engine.linksForIdentifier("Tracking")
-
-        # If help is available for this keyword, get the help data
-        # of the first file reference.
-        if links:
-            helpdata = engine.fileData(links.constBegin().value())
-            # show the documentation to the user
-            if not helpdata.isEmpty():
-                from PyQt4.QtCore import pyqtRemoveInputHook; pyqtRemoveInputHook()
-                import pdb; pdb.set_trace()
+        splitter.insertWidget(0, hengine.contentWidget())
+        splitter.insertWidget(1, hbrowser)
 
 
 if __name__ == "__main__":
