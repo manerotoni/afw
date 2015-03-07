aacb60f00e4dc25396d60347ae496d24a456c6a9
rhoef <rudolf.hoefler@gmail.com>
2015-03-07 03:38:21 +0100

    cat/imageio/__init__.py

--- cat/imageio/__init__.py
+++ cat/imageio/__init__.py
@@ -3,12 +3,7 @@ __init__.py
 """
 
 __author__ = 'rudolf.hoefler@gmail.com'
-__copyright__ = ('The CellCognition Project'
-                 'Copyright (c) 2006 - 2012'
-                 'Gerlich Lab, IMBA Vienna, Austria'
-                 'see AUTHORS.txt for contributions')
-__licence__ = 'LGPL'
-__url__ = 'www.cellcognition.org'
+__licence__ = 'GPL'
 
 
 from .imageio import *
