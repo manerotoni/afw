682aa66dc9c6289b939c853241242e8cb82eb15e
rhoef <rudolf.hoefler@gmail.com>
2015-03-08 18:42:12 +0100

    VC

--- cat/version.py
+++ cat/version.py
@@ -15,8 +15,8 @@ version = "%s.%s%s" %(MAJOR, MINOR, "-alpha")
 appstr = '%s-%s' %(appname, version)
 
 information = ('<p style="font-size:xx-large;color:white"><b>%s %s</b></p><br>'
+               '<p style="color:white">Copyright (c) 2014</p>'
                '<p style="line-height:25px;font-size:x-large;color:white">'
-               'Copyright (c) 2014<br>'
                'Gerlich LAB - IMBA<br>'
                'Vienna Biocenter Campus</p>'
                '<p style="color:white">'
